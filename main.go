05970d376645e1869eae309ac94d62f1a2a813ab
Marco Palumbo <36733571+marcopalumbo@users.noreply.github.com>
2022-11-17 15:16:35 +0100

    Message text update

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "It's alright Bright"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
